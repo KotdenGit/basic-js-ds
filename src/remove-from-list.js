3fdc6c9319ccd4ad2d621dfe2f6049bf9e830ad1
KotdenGit <kotden09@gmail.com>
2023-12-26 02:09:13 +0300

    feat: remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next
+  }
+
+  let prev = l
+  let next = prev.next
+
+  while (next) {
+    if (next.value === k) {
+      prev.next = next.next
+    } else {
+      prev = next
+    }
+    next = prev.next
+  }
+
+  return l
 }
 
 module.exports = {
